ac1f73c44ec0b3e6b12f6370433887c282f45937
gplatono <gr.plat@gmail.com>
2017-03-14 08:22:24 -0400

    a

--- blender_project/eval.py
+++ blender_project/eval.py
@@ -7,6 +7,7 @@ for line in open("dump", "r").readlines():
 input = input.split("###\n")
 tests = []
 for test in input:
-    test = test.split(':')
-    tests.append([test[0], test[1], test[2], [x for x in re.split("[;\n]", test[3]) if x != ""]])
+    if test != "":
+        test = test.split(':')
+        tests.append([test[0], test[1], test[2], [x for x in re.split("[;\n]", test[3]) if x != ""]])
 print (tests)
