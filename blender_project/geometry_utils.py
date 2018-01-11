3caa3f4b4ac77db9cce86045f125fa2634d2f5a4
gplatono <gr.plat@gmail.com>
2018-01-11 17:58:35 -0500

    1/11/2018

--- blender_project/geometry_utils.py
+++ blender_project/geometry_utils.py
@@ -1,34 +1,54 @@
 import math
 import numpy
 
+#Computes the cross-product of vectors a and b
+#Inputs: a,b - vector coordinates as tuples or lists
+#Return value: a triple of coordinates
 def cross_product(a, b):
     return (a[1] * b[2] - a[2] * b[1], b[0] * a[2] - b[2] * a[0],
             a[0] * b[1] - a[1] * b[0])
 
+#Given three points that define a plane, computes the normal vector to that plane
+#Inputs: a,b,c - point coordinates as tuples or lists
+#Return value: normal vector as a triple of coordinates
 def get_normal(a, b, c):
     return cross_product((a[0] - b[0], a[1] - b[1], a[2] - b[2]),
                          (c[0] - b[0], c[1] - b[1], c[2] - b[2]))
 
+#Given a point and a plane defined by a, b and c
+#computes the orthogonal distance from the point to that plane
+#Inputs: point,a,b,c - point coordinates as tuples or lists
+#Return value: the distance value
 def get_distance_from_plane(point, a, b, c):
     normal = numpy.array(get_normal(a, b, c))
     return math.fabs((numpy.array(point).dot(normal) - numpy.array(a).dot(normal)) / numpy.linalg.norm(normal))
 
-#distance from x3 to the line connecting x1 to x2
+#Computes the orthogonal distance between x3 and the line
+#defined by x1 and x2
+#Inputs: x1, x2, x3 - point coordinates as tuples or lists
+#Return value: the distance value
 def get_distance_from_line(x1, x2, x3):
     t = (x3[0] - x1[0]) * (x2[0] - x1[0]) + (x3[1] - x1[1]) * (x2[1] - x1[1]) * (x3[2] - x1[2]) * (x2[2] - x1[2])
     t = t / (point_distance(x1, x2) ** 2)
     x0 = (x1[0] + (x2[0] - x1[0]) * t, x1[1] + (x2[1] - x1[1]) * t, x1[2] + (x2[2] - x1[2]) * t)
     return point_distance(x0, x3)
 
-#Euclidean distance between two points
+#Computes a simple Euclidean distance between two points
+#Inputs: a, b - point coordinates as tuples or lists
+#Return value: the distance value
 def point_distance(a, b):
     return numpy.linalg.norm(numpy.array(a) - numpy.array(b))
 
+
+#Computes the projection of the bounding box of a set
+#of points onto the XY-plane
+#Inputs: a, b - point coordinates as tuples or lists
+#Return value: the distance value
 def get_2d_bbox(points):
     min_x = 1e9
     min_y = 1e9
     max_x = -1e9
-    max_y = -1e9
+    max_y = -1e9    
     for p in points:
         min_x = min(min_x, p[0])
         min_y = min(min_y, p[1])
