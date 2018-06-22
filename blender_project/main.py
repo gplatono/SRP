1b4053331cf674fede0314fe3c1a8f351b945f1d
gplatono <gr.plat@gmail.com>
2018-06-22 16:57:36 -0400

    06/22/2017

--- blender_project/main.py
+++ blender_project/main.py
@@ -20,6 +20,7 @@ sys.path.insert(0, filepath)
 from entity import Entity
 from geometry_utils import *
 from annot_parser import *
+from spatial import *
 
 
 link = False
@@ -88,9 +89,6 @@ rf_mapping = {'to the left of': 'to_the_left_of_deic',
 
 #The list of entities
 entities = []
-
-#The observer object (camera)
-observer = None
 
 #Average distance between entities in the scene
 avg_dist = 0
@@ -192,88 +190,6 @@ def get_frame_size():
     return max(max_x - min_x, max_y - min_y, max_z - min_z)
 
 
-#Raw metric for the nearness relation
-#Doesn't take into account the nearness statistics in the scene
-#Inputs: a, b - entities
-#Return value: real number from [0, 1], the raw nearness measure
-def near_raw(a, b):
-    bbox_a = a.get_bbox()
-    bbox_b = b.get_bbox()
-    dist = dist_obj(a, b)
-    max_dim_a = max(bbox_a[7][0] - bbox_a[0][0],
-                    bbox_a[7][1] - bbox_a[0][1],
-                    bbox_a[7][2] - bbox_a[0][2])
-    max_dim_b = max(bbox_b[7][0] - bbox_b[0][0],
-                    bbox_b[7][1] - bbox_b[0][1],
-                    bbox_b[7][2] - bbox_b[0][2])
-    if a.get('planar') is not None:
-        #print ("TEST", a.name, b.name)
-        dist = min(dist, get_planar_distance_scaled(a, b))
-    elif b.get('planar') is not None:
-        dist = min(dist, get_planar_distance_scaled(b, a))        
-    elif a.get('vertical_rod') is not None or a.get('horizontal_rod') is not None or a.get('rod') is not None:
-        dist = min(dist, get_line_distance_scaled(a, b))
-    elif b.get('vertical_rod') is not None or b.get('horizontal_rod') is not None or b.get('rod') is not None:
-        dist = min(dist, get_line_distance_scaled(b, a))
-    elif a.get('concave') is not None or b.get('concave') is not None:
-        dist = min(dist, closest_mesh_distance_scaled(a, b))
-
-    fr_size = get_frame_size()
-    raw_metric = e ** (- 0.05 * dist)
-    '''0.5 * (1 - min(1, dist / avg_dist + 0.01) +'''    
-    return raw_metric * (1 - raw_metric / fr_size)
-
-#Computes the nearness measure for two entities
-#Takes into account the scene statistics:
-#The raw nearness score is updated depending on whether one object is the closest to another
-#Inputs: a, b - entities
-#Return value: real number from [0, 1], the nearness measure
-def near(a, b):
-    raw_near_a = []
-    raw_near_b = []
-    raw_near_measure = near_raw(a, b)
-    for entity in entities:
-        if entity != a and entity != b:
-            near_a_entity = near_raw(a, entity)
-            near_b_entity = near_raw(b, entity)
-            #print (entity.name, near_a_entity, near_b_entity)
-            #if dist_a_to_entity < raw_dist:
-            raw_near_a += [near_a_entity]
-            #if dist_b_to_entity < raw_dist:
-            raw_near_b += [near_b_entity]
-    #print ("RAW:", a.name, b.name, raw_near_measure)
-    average_near_a = sum(raw_near_a) / len(raw_near_a)
-    average_near_b = sum(raw_near_b) / len(raw_near_b)
-    avg_near = 0.5 * (average_near_a + average_near_b)
-    max_near_a = max(raw_near_a)
-    max_near_b = max(raw_near_b)
-    max_near = max(raw_near_measure, max_near_a, max_near_b)
-    #print ("AVER: ", average_near_a, average_near_b)
-    ratio = raw_near_measure / max_near
-    if (raw_near_measure < avg_near):
-        near_measure_final = 0.5 * raw_near_measure
-    else:        
-        near_measure_final = raw_near_measure * ratio
-    near_measure = raw_near_measure + (raw_near_measure - avg_near) * min(raw_near_measure, 1 - raw_near_measure)
-    print ("RAW: {}; NEAR: {}; FINAL: {}; AVER: {};".format(raw_near_measure, near_measure, near_measure_final, (average_near_a + average_near_b) / 2))
-    return near_measure
-
-#Computes the between relation (a is between b and c)
-#Inputs: a, b, c - entities
-#Return value: real number from [0, 1]
-def between(a, b, c):
-    bbox_a = a.get_bbox()
-    bbox_b = a.get_bbox()
-    bbox_c = c.get_bbox()
-    center_a = a.get_bbox_centroid()
-    center_b = b.get_bbox_centroid()
-    center_c = c.get_bbox_centroid()
-    vec1 = np.array(center_b) - np.array(center_a)
-    vec2 = np.array(center_c) - np.array(center_a)
-    cos = np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2) + 0.001)
-    dist = get_distance_from_line(center_b, center_c, center_a) / max(max(a.dimensions), max(b.dimensions), max(c.dimensions))
-    
-    return math.exp(- 2 * math.fabs(-1 - cos))
 
 
 #Computes the degree of vertical alignment (coaxiality) between two entities
@@ -302,113 +218,6 @@ def v_offset(a, b):
     h_dist = math.sqrt((center_a[0] - center_b[0]) ** 2 + (center_a[1] - center_b[1]) ** 2)    
     return gaussian(2 * (center_a[2] - center_b[2] - 0.5*(dim_a[2] + dim_b[2])) /  \
                     (1e-6 + dim_a[2] + dim_b[2]), 0, 1 / math.sqrt(2*pi))
-
-#Computes the "larger-than" relation
-#Inputs: a, b - entities
-#Return value: real number from [0, 0.5]
-def larger_than(a, b):
-    bbox_a = a.get_bbox()
-    bbox_b = b.get_bbox()
-    return 1 / (1 + e ** (bbox_b[7][0] - bbox_b[0][0] \
-                          + bbox_b[7][1] - bbox_b[0][1] \
-                          + bbox_b[7][2] - bbox_b[0][2] \
-                          - (bbox_a[7][0] - bbox_a[0][0] \
-                             + bbox_a[7][1] - bbox_a[0][1] \
-                             + bbox_a[7][2] - bbox_a[0][2])))
-
-
-#Computes the "on" relation
-#Inputs: a, b - entities
-#Return value: real number from [0, 1]
-def on(a, b):
-    ret_val = 0.5 * (above(a, b) + touching(a, b))
-    if b.get('planar') is not None and larger_than(b, a) and a.centroid[2] > 0.5 * a.dimensions[2]:
-        ret_val = max(ret_val, touching(a, b))    
-    #ret_val = 0.5 * (v_offset(a, b) + get_proj_intersection(a, b))
-    #print ("ON {}, {}, {}".format(ret_val, get_proj_intersection(a, b), v_offset(a, b)))
-    #ret_val = max(ret_val, 0.5 * (above(a, b) + touching(a, b)))
-    #print ("ON {}".format(ret_val))
-    for ob in b.constituents:
-        ob_ent = Entity(ob)
-        if ob.get('working_surface') is not None or ob.get('planar') is not None:
-            ret_val = max(ret_val, 0.5 * (v_offset(a, ob_ent) + get_proj_intersection(a, ob_ent)))
-            ret_val = max(ret_val, 0.5 * (int(near(a, ob_ent) > 0.99) + larger_than(ob_ent, a)))
-    if b.get('planar') is not None and isVertical(b):
-        ret_val = max(ret_val, math.exp(- 0.5 * get_planar_distance_scaled(a, b)))
-    return ret_val
-
-#Computes the "over" relation
-#Currently, the motivation behind the model is that
-#one object is considered to be over the other
-#iff it's above it and relatively close to it.
-#Inputs: a, b - entities
-#Return value: real number from [0, 1]
-def over(a, b):
-    bbox_a = a.get_bbox()
-    bbox_b = b.get_bbox()
-    return 0.5 * above(a, b) + 0.2 * get_proj_intersection(a, b) + 0.3 * near(a, b)
-
-
-#Computes the "under" relation, which is taken to be symmetric to "over"
-#Inputs: a, b - entities
-#Return value: real number from [0, 1]
-def under(a, b):
-    return over(b, a)
-
-
-#Computes the "closer-than" relation
-#Inputs: a, b - entities
-#Return value: real number from [0, 1]
-def closer_than(a, b, pivot):
-    return int(point_distance(a.centroid, pivot.centroid) < point_distance(b.centroid, pivot.centroid))
-
-
-#Computes the deictic version of the "in-front-of" relation
-#For two objects, one is in front of another iff it's closer and
-#between the observer and that other object
-#Inputs: a, b - entities
-#Return value: real number from [0, 1]
-def in_front_of_deic(a, b):
-#def in_front_of_extr(a, b, observer):
-    bbox_a = a.get_bbox()
-    max_dim_a = max(bbox_a[7][0] - bbox_a[0][0],
-                    bbox_a[7][1] - bbox_a[0][1],
-                    bbox_a[7][2] - bbox_a[0][2]) + 0.0001
-    dist = get_distance_from_line(observer.centroid, b.centroid, a.centroid)
-    #print ("{}, {}, CLOSER: {}, WC_DEIC: {}, WC_EXTR: {}, DIST: {}".format(a.name, b.name, closer_than(a, b, observer), within_cone(b.centroid - observer.centroid, a.centroid - observer.centroid, 0.95), within_cone(b.centroid - a.centroid, Vector((0, -1, 0)) - a.centroid, 0.8), e ** (- 0.1 * get_centroid_distance_scaled(a, b))))
-    return e ** (- 0.01 * get_centroid_distance_scaled(a, b)) * within_cone(b.centroid - a.centroid, Vector((1, 0, 0)), 0.7)
-    '''0.3 * closer_than(a, b, observer) + \
-                  0.7 * (max(within_cone(b.centroid - observer.centroid, a.centroid - observer.centroid, 0.95),
-                  within_cone(b.centroid - a.centroid, Vector((1, 0, 0)), 0.7)) * \
-                  e ** (- 0.2 * get_centroid_distance_scaled(a, b)))#e ** (-dist / max_dim_a))'''
-
-#Enable SVA
-#Computes the deictic version of the "behind" relation
-#which is taken to be symmetric to "in-front-of"
-#Inputs: a, b - entities
-#Return value: real number from [0, 1]
-def behind_deic(a, b):
-    return in_front_of_deic(b, a)
-
-'''
-def bbox_inside_test(a, b):
-    shared_volume = get_bbox_intersection(a, b)
-    return shared_volume / (b.dimensions[0] * b.dimensions[1] * b.dimensions[2] + 0.001)
-'''
-
-#Computes the "at" relation
-#Inputs: a, b - entities
-#Return value: real number from [0, 1]
-def at(a, b):
-    return 0.8 * near(a, b) + 0.2 * touching(a, b)
-
-
-def inside(a, b):
-    a_bbox = a.bbox
-    b_bbox = b.bbox
-    shared_volume = get_bbox_intersection(a, b)
-    proportion = shared_volume / b.volume
-    return sigmoid(proportion, 1.0, 1.0)
 
     
 #The following functions are for precomputing the corresponding
@@ -641,33 +450,6 @@ def vp_project(entity, observer):
     pixel_coords = [(round(point.x * render_size[0]),round(point.y * render_size[1]),) for point in co_2d]
     return pixel_coords
 
-#Computes the "touching" relation
-#Two entities are touching each other if they
-#are "very close"
-#Inputs: a, b - entities
-#Return value: real number from [0, 1]
-def touching(a, b):
-    bbox_a = a.get_bbox()
-    bbox_b = b.get_bbox()
-    center_a = a.get_bbox_centroid()
-    center_b = b.get_bbox_centroid()
-    rad_a = max(bbox_a[7][0] - bbox_a[0][0], \
-                bbox_a[7][1] - bbox_a[0][1], \
-                bbox_a[7][2] - bbox_a[0][2]) / 2
-    rad_b = max(bbox_b[7][0] - bbox_b[0][0], \
-                bbox_b[7][1] - bbox_b[0][1], \
-                bbox_b[7][2] - bbox_b[0][2]) / 2
-    
-    '''for point in bbox_a:
-        if point_distance(point, center_b) < rad_b:
-            return 1
-    for point in bbox_b:
-        if point_distance(point, center_a) < rad_a:
-            return 1'''
-    mesh_dist = 1e9
-    if get_centroid_distance_scaled(a, b) <= 1.5:
-        mesh_dist = closest_mesh_distance_scaled(a, b)
-    return math.exp(- 5 * mesh_dist)
 
 #Filters the entities list according to the set of constraints, i.e.,
 #returns the list of entities satisfying certain criteria
@@ -768,71 +550,6 @@ def get_weighted_measure(a, b, observer):
     distance_factor = math.exp(-0.01 * axial_dist[0])
     return 0.5 * horizontal_component + 0.3 * vertical_component + 0.2 * distance_factor
 
-#Computes a special function that takes a maximum value at cutoff point
-#and decreasing to zero with linear speed to the left, and with exponetial speed to the right
-#Inputs: x - position; cutoff - maximum point; left, right - degradation coeeficients for left and
-#right sides of the function
-#Return value: real number from [0, 1]
-def asym_inv_exp(x, cutoff, left, right):
-    return math.exp(- right * math.fabs(x - cutoff)) if x >= cutoff else max(0, left * (x/cutoff) ** 3)
-
-#Symmetric to the asym_inv_exp.
-#Computes a special function that takes a maximum value at cutoff point
-#and decreasing to zero with linear speed to the RIGHT, and with exponetial speed to the LEFT
-#Inputs: x - position; cutoff - maximum point; left, right - degradation coeeficients for left and
-#right sides of the function
-#Return value: real number from [0, 1]
-def asym_inv_exp_left(x, cutoff, left, right):
-    return math.exp(- left * (x - cutoff)**2) if x < cutoff else max(0, right * (x/cutoff) ** 3)
-
-#Computes the deictic version of to-the-right-of relation
-#Inputs: a, b - entities
-#Return value: real number from [0, 1]
-def to_the_right_of_deic(a, b):
-    a_bbox = get_2d_bbox(vp_project(a, observer))
-    b_bbox = get_2d_bbox(vp_project(b, observer))
-    axial_dist = scaled_axial_distance(a_bbox, b_bbox)
-    if axial_dist[0] <= 0:
-        return 0
-    horizontal_component = asym_inv_exp(axial_dist[0], 1, 1, 0.05)#sigmoid(axial_dist[0], 2.0, 5.0) - 1.0
-    vertical_component = math.exp(- 0.5 * math.fabs(axial_dist[1]))
-    distance_factor = math.exp(- 0.1 * axial_dist[0])
-    #print ("Hor:", horizontal_component, "VERT:", vertical_component, "DIST:", distance_factor)
-    weighted_measure = 0.5 * horizontal_component + 0.5 * vertical_component #+ 0.1 * distance_factor
-    return weighted_measure
-
-#Computes the deictic version of to-the-left-of relation
-#Inputs: a, b - entities
-#Return value: real number from [0, 1]
-def to_the_left_of_deic(a, b):
-    return to_the_right_of_deic(b, a)
-
-#Computes the above relation
-#Inputs: a, b - entities
-#Return value: real number from [0, 1]
-def above(a, b):
-    #bbox_a = a.get_bbox()
-    #bbox_b = b.get_bbox()
-    #span_a = a.get_span()
-    #span_b = b.get_span()
-    #center_a = a.get_bbox_centroid()
-    #center_b = b.get_bbox_centroid()
-    #scaled_vertical_distance = (center_a[2] - center_b[2]) / ((span_a[5] - span_a[4]) + (span_b[5] - span_b[4]))
-    return within_cone(a.centroid - b.centroid, Vector((0, 0, 1)), 0.05) * e ** (- 0.01 * get_centroid_distance_scaled(a, b))
-
-#Computes the below relation, which is taken to be symmetric to above
-#Inputs: a, b - entities
-#Return value: real number from [0, 1]
-def below(a, b):
-    return above(b, a)
-
-#STUB
-def in_front_of_intr(a, b):
-    pass
-
-#STUB
-def behind_intr(a, b):
-    in_front_of_intr(b, a)
 
 
 def fix_ids():
@@ -877,6 +594,9 @@ def pick_descriptions(relatum):
     max_vals = [item[0] for item in max_vals]
     return tuple(max_vals[0:3])
 
+#The observer object (camera)
+observer = get_observer()
+
 #Entry point
 #Implementation of the evaluation pipeline
 def main():
@@ -890,8 +610,6 @@ def main():
         avg_dist = avg_dist * 2 / (len(entities) * (len(entities) - 1))
 
     #print (filepath, sys.path)
-    global observer
-    observer = get_observer()
     if "--" in sys.argv:
         args = sys.argv[sys.argv.index("--") + 1:]
         init_parser([entity.name for entity in entities])
