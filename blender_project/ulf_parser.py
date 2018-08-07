da213b5578a30d6f845d3766971d488ed0f7e289
gplatono <gr.plat@gmail.com>
2018-08-07 15:11:19 -0400

    08/07/2018

---
+++ blender_project/ulf_parser.py
@@ -0,0 +1,17 @@
+
+
+class TreeNode(object):
+    def __init__(self, content, children):
+        self.content = content
+        self.children = children
+
+class ULF_Parser(object):
+    def __init__(self):
+        pass
+
+    def parse(input):
+        if input[0] == '(':
+            TreeNode
+            
+
+    
